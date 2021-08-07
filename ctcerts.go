d8c80732a6138b0f3766006ee53aa2e0e0676cf3
k.koromyslov <k.koromyslov@rambler-co.ru>
2021-08-07 14:29:13 +0300

    initial commit

---
+++ ctcerts.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func main() {
+	err := realMain()
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func realMain() error {
+	if len(os.Args) != 7 {
+		return nil
+	}
+
+	// savetofile <mode> <filepath> <uid> <gid> <perm> <data>
+	mode := os.Args[1]
+	path := os.Args[2]
+	uid_arg := os.Args[3]
+	gid_arg := os.Args[4]
+	perm_arg := os.Args[5]
+	data := os.Args[6]
+
+	uid, err := strconv.Atoi(uid_arg)
+	if err != nil {
+		return err
+	}
+
+	gid, err := strconv.Atoi(gid_arg)
+	if err != nil {
+		return err
+	}
+
+	perm_u, err := strconv.ParseUint(perm_arg, 8, 32)
+	if err != nil {
+		return err
+	}
+
+	perm := os.FileMode(perm_u)
+
+	switch mode {
+	case "append":
+		f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, perm)
+		if err != nil {
+			return err
+		}
+
+		defer f.Close()
+
+		if _, err = f.WriteString(data); err != nil {
+			return err
+		}
+	case "append-nl":
+		f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, perm)
+		if err != nil {
+			return err
+		}
+
+		defer f.Close()
+
+		if _, err = f.WriteString(data); err != nil {
+			return err
+		}
+
+		if _, err = f.WriteString("\n"); err != nil {
+			return err
+		}
+	case "create-nl":
+		err := ioutil.WriteFile(path, append([]byte(data), []byte("\n")...), perm)
+		if err != nil {
+			return err
+		}
+	default: // "create"
+		if _, err := os.Stat(path); err == nil {
+			file_origin, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			if !bytes.Equal(file_origin, []byte(data)) {
+				err := ioutil.WriteFile(path, []byte(data), perm)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			dirPath := filepath.Dir(path)
+
+			if _, serr := os.Stat(dirPath); serr != nil {
+				merr := os.MkdirAll(dirPath, os.ModePerm)
+				if merr != nil {
+					panic(merr)
+				}
+			}
+
+			err := ioutil.WriteFile(path, []byte(data), perm)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	err = os.Chown(path, uid, gid)
+	if err != nil {
+		return err
+	}
+
+	err = os.Chmod(path, perm)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
